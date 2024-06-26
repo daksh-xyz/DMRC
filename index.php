8452334e7f1fa1cabc0c6ecf4c58833cf90fc98c
daksh-xyz <dakshuklal@gmail.com>
2024-06-26 18:42:03 +0530

    new changes: > responsive > favicon > register/ forgot pass > language selector > color changes

--- index.php
+++ index.php
@@ -5,47 +5,76 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/styles.css">
-    <title>Dmrc Alumni Portal</title>
-</he    ad>
-      
-  <body>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <title>DMRC | Alumni Portal</title>
+</head>
+
+<body>
     <header>
         <div class="head">
-            <img src="./assets/images/logo.png" alt="">
-            <h1>DMRC ALUMNI PORTAL</h1>
+            <img src="./assets/images/logo.png" width="100px" height="39px">
+            <form action="#">
+                <select class="language">
+                    <option value="English">English</option>
+                    <option value="Hindi">Hindi</option>
+                </select>
+            </form>
         </div>
     </header>
 
-    <div class="container">
-        <div class="about">
-            <h1 id="aboutH1">DMRC</h1>
-            <h2 id="aboutH2">ALUMNI PORTAL</h2>
-            <p>
-                Welcome to the official alumni portal of the Delhi Metro Rail Corporation. This platform is dedicated to
-                reconnecting our past employees, fostering professional networks, and celebrating the accomplishments of
-                our alumni community. Join us in staying connected and contributing to the growth and success of our
-                alumni family.
-            </p>
-        
-        </div>
-<div class="login-form">
-            <h2>Alumni Login</h2>
-            <form action="#">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" required>
+    <div class="center">
+        <div class="container">
+            <div class="about">
+                <h1 id="aboutH1">DMRC</h1>
+                <h2 id="aboutH2">Alumni Portal</h2>
+                <p>
+                    Welcome to the official alumni portal of the Delhi Metro Rail Corporation. This platform is
+                    dedicated to
+                    reconnecting our past employees, fostering professional networks, and celebrating the
+                    accomplishments of
+                    our alumni community. Join us in staying connected and contributing to the growth and success of our
+                    alumni family.
+                </p>
 
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="login-form">
+                <div class="login-head">
+                    <h2>Login</h2>
+                    <p>Welcome! Please enter the details to login.</p>
+                </div>
 
-                <input type="submit" value="Login">
-            </form>
+                <form action="#">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+                    <br>
+                    <div class="pcenter">
+                        <input type="submit" value="Login">
+                    </div>
+                </form>
+                <br>
+                <div class="pcenter">
+                    <div class="fpass">
+                        <span>Forgot Password? <a href="./pages/ForgotPass.php">Reset Password</a></span>
+                    </div>
+                </div>
+                <br>
+                <div class="pcenter">
+                    <div class="reg-button">
+                        <a href="./pages/Register.php">Register As New User</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-    <footer>
-        <p>&copy; 2024 Dmrc Alumni Portal . All rights reserved.</p>
-    </footer>
-
 </body>
 
 </html>
