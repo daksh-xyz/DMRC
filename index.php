2c770140e57204eeecf4cca1831235257860fb6e
daksh-xyz <dakshuklal@gmail.com>
2024-06-26 15:42:28 +0530

    sync

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DMRC | Alumni Portal</title>
 </head>
+
 <body>
     <h1>Connecting Alumnis</h1>
+    <img src="./assets/images/img.png" alt="">
 </body>
+
 </html>
 
 hello
