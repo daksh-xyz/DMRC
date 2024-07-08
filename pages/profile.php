317a60dbedb0d10e231066a7480878826c1aa75c
tejasitis <tejastripathi30@gmail.com>
2024-07-08 14:15:13 +0530

    update

--- pages/profile.php
+++ pages/profile.php
@@ -14,7 +14,7 @@
     include ("../php/config.php");
     if (!isset($_SESSION['id'])) {
         header("Location: ../index.php");
-    }
+    }else{
     $id = $_SESSION['id'];
 
     if (isset($_POST["pfpUpdate"])) {
@@ -295,6 +295,7 @@
             event.currentTarget.classList.add('active');
         }
     </script>
+    <?php }?>
 </body>
 
 </html>
